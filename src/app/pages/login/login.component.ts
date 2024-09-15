6e442a59a4c33d972118f8e106ba42fbdd0920fd
abhi12M <emailabhishekmittal@gmail.com>
2024-09-15 12:17:56 +0530

    login and home page added

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,34 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterOutlet } from '@angular/router';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [RouterOutlet, CommonModule, ReactiveFormsModule, FormsModule, MatSnackBarModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private router: Router, private snackBar: MatSnackBar) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.value.email == 'abhishek@gmail.com' && this.loginForm.value.password == 'qwerty') {
+      this.router.navigate(['/home']);
+    } else {
+      this.snackBar.open('Invalid credentials!', 'Close', {
+        duration: 3000,
+      });
+    }
+  }
+}
